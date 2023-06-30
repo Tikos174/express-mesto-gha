5044b5054909acab7432f40ccf0b63633f4e60cb
Deryabin Aleksey <deryadinalex@bk.ru>
2023-06-30 21:38:28 +0300

    fix

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
