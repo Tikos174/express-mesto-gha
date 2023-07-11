0d6edb6d93e53bdfed53797ca4f97c1a26c2b85c
Deryabin Aleksey <deryadinalex@bk.ru>
2023-07-11 23:08:49 +0300

    fix

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -12,15 +13,20 @@ const auth = require('./middlewares/auth');
 const NotFound = require('./utils/notFoundErr');
 const errorHandler = require('./middlewares/errorHandler');
 
+const { requestLog, errorLog } = require('./middlewares/loggers');
+
 const avatar = /^https?:\/\/[www.]?[\w\-._~:/?#[\]@!$&'()*+,;=%]+\.[\w\-._~:/?#[\]@!$&'()*+,;=%]+#?$/;
 const password = /^[a-zA-z0-9]{8,}$/;
 
-const app = express();
+const { PORT = 3000 } = process.env;
+// const { PORT = 3001 } = process.env;
+const app = express(process.env.JWT_SECRET);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(requestLog);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -64,9 +70,9 @@ app.use(auth, (req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(errorLog);
 app.use(errors());
 
 app.use(errorHandler);
 
-app.listen(3000);
-// app.listen(3002);
+app.listen(PORT);
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,13 +6,17 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new NotАuthorized('Необходима авторизация'));
+  }
+
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new NotАuthorized('Необходима авторизация'));
+    return next(new NotАuthorized('Необходима авторизация'));
   }
 
   req.user = payload;
